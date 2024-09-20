e1e44de9a9026100a6732fa4187d12a18ffe78c2
ifeelomii <omkarware003@gmail.com>
2024-09-20 04:44:47 -0400

    Implemeted a search box

---
+++ src/components/Typewriter.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const Typewriter=() =>{
+    const [value,setValue] = useState('Start Searching !');
+    const [count,setCount] = useState(0);
+
+    const handleInput = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleClick = () => {
+        if(count == 0) setValue("");
+        setCount(count+1);
+    };
+
+    const handleBlur=()=>{
+
+    };
+
+    const submitQuery =(e)=>{
+        console.log(`Output: ${e.target.value} `)
+    }
+    return(
+    <>
+        <div 
+            contentEditable 
+            className="typewriter board" 
+            onChange={handleInput}
+            suppressContentEditableWarning={true}
+        > 
+            <input type="text" contentEditable id="search-box" value={value} onBlur={handleBlur} onClick={handleClick} onChange={handleInput} placeholder="Enter text here"/>
+        </div>
+    </>);
+};
+
+export default Typewriter;
